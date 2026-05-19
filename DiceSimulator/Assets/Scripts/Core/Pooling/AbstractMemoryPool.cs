03522b5d47534285a475aaf26121929b996bd552
Radek <radekkozaczuk1@gmail.com>
2026-05-19 21:10:00 +0200

    adjustments

--- DiceSimulator/Assets/Scripts/Core/Pooling/AbstractMemoryPool.cs
+++ DiceSimulator/Assets/Scripts/Core/Pooling/AbstractMemoryPool.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine.Assertions;
 
 namespace Core.Pooling
 {
@@ -8,7 +7,6 @@ namespace Core.Pooling
     {
         public int MaxSize
         {
-            get => _maxSize;
             set
             {
                 if (value < _maxSize && value < _stack.Count)
@@ -21,6 +19,7 @@ namespace Core.Pooling
 
         int _maxSize;
 
+        // ReSharper disable once CollectionNeverUpdated.Local
         readonly Stack<T> _stack = new();
         Action<T, bool>? _onReturn;
         protected readonly object _locker = new();
@@ -32,27 +31,6 @@ namespace Core.Pooling
         }
 
         public void Dispose() { }
-
-        /// <summary>
-        /// Returns element back to the pool if <see cref="MaxSize"/> has not been reached.
-        /// Invokes <see cref="_onReturn"/> function.
-        /// </summary>
-        public void Return(T element)
-        {
-            if (_stack.Count >= MaxSize)
-            {
-                _onReturn?.Invoke(element, true);
-
-                return;
-            }
-
-            _onReturn?.Invoke(element, false);
-
-            lock (_locker)
-                Assert.IsFalse(_stack.Contains(element), "Attempted to return the same element to the pool twice!");
-
-            _stack.Push(element);
-        }
 
         // We assume here that we're in a lock
         protected T GetInternal() => _stack.Count == 0 ? Alloc() : _stack.Pop();
